edef9ccb755cb3a0fc99e637946c1c9232831d39
awathey <54956886+awathey@users.noreply.github.com>
2019-09-05 13:41:35 -0500

    Add files via upload

---
+++ MyFirstApp_Athey-250s01/MyFirstConsole_Athey-250s01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsole_Athey_250s01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello IS250");Console.ReadLine();
+
+        }
+    }
+}
